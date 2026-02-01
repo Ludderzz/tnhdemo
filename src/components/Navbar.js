3e1f48bff461c2e320a1230bfb873cb06aab4bf1
Eddie Ludlam <eludlam1221@gmail.com>
2026-02-01 00:18:35 +0000

    updated nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import DarkModeToggle from "./DarkModeToggle";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="logo">TNH</div>
-      <ul className="nav-links">
+
+      {/* Hamburger button */}
+      <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+        <div className={isOpen ? "bar open" : "bar"}></div>
+        <div className={isOpen ? "bar open" : "bar"}></div>
+        <div className={isOpen ? "bar open" : "bar"}></div>
+      </div>
+
+      {/* Nav links */}
+      <ul className={isOpen ? "nav-links mobile-open" : "nav-links"}>
         <li>Home</li>
         <li>Services</li>
         <li>Impact</li>
         <li>Get Involved</li>
         <li>Donate</li>
       </ul>
+
+      <DarkModeToggle />
     </nav>
   );
 };
